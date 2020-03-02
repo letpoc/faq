9cfb835d050506101957f9022492861f83b59fa8
chetanbakshi <chetan.bakshi@gmail.com>
2020-03-02 07:55:46 +0530

    faq project initialization

---
+++ service/src/main/java/com/faq/FaqApplication.java
@@ -0,0 +1,14 @@
+package com.faq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaqApplication.class, args);
+		System.out.println("Hello 5"); 
+	}
+
+}
